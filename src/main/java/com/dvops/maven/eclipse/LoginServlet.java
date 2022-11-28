ffbd0a37c88b924a01b13048f747eec656f1fe9d
carolyn2004 <2102332J@student.tp.edu.sg>
2022-11-28 10:26:55 +0800

    [fix](login): Fixed bug in login servlet
    - Parameter("YourUserName") was spelt incorrectly
    - It was spelt as yourUserName in LoginServlet
    - This caused the values keyed to not be passed to LoginServlet
    - Successful login was also redirected to wrong page
    - It was redirected to home.jsp which does not exist
    - I corrected it from home.jsp to index.jsp
    
    Reviewed by: Carolyn
    
    Signed-off-by: carolyn2004 <2102332J@student.tp.edu.sg>

--- src/main/java/com/dvops/maven/eclipse/LoginServlet.java
+++ src/main/java/com/dvops/maven/eclipse/LoginServlet.java
@@ -16,65 +16,76 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 /**
  * Servlet implementation class LoginServlet
  */
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.setContentType("text/html");
-		//Step 1: Initialize a PrintWriter object to return the html values via the response
+		// Step 1: Initialize a PrintWriter object to return the html values via the
+		// response
 		PrintWriter out = response.getWriter();
-		//Step 2: retrieve the four parameters from the request from the web form
-		String n = request.getParameter("yourUserName");
-		String p = request.getParameter("yourPassword");
-		//Step 3: attempt connection to database using JDBC, you can change the username and password accordingly using the phpMyAdmin > User Account dashboard
+		// Step 2: retrieve the four parameters from the request from the web form
+		String n = request.getParameter("YourUserName");
+		String p = request.getParameter("YourPassword");
+		System.out.println(n);
+		System.out.println(p);
+		// Step 3: attempt connection to database using JDBC, you can change the
+		// username and password accordingly using the phpMyAdmin > User Account
+		// dashboard
 		try {
-		 Class.forName("com.mysql.cj.jdbc.Driver");
-		 Connection con = DriverManager.getConnection(
-		 "jdbc:mysql://localhost:3308/userdetails", "root", "password");
-		//Step 4: implement the sql query using prepared statement (https://docs.oracle.com/javase/tutorial/jdbc/basics/prepared.html)
-		 PreparedStatement ps = con.prepareStatement("select username from USERDETAILS where username=? and password=?");
-		//Step 5: parse in the data retrieved from the web form request into the prepared statement accordingly
-		 ps.setString(1, n);
-		 ps.setString(2, p);
-		//Step 6: perform the query on the database using the prepared statement
-		 ResultSet rs = ps.executeQuery();
-		//Step 7: check if the query had been successfully execute, return “You are successfully registered” via the response,
-		 if (rs.next()){
-		RequestDispatcher rd = request.getRequestDispatcher("home.jsp");
-		rd.forward(request,response);
-		 } 
-		 else
-		 {
-			 out.println("Login Failed!");
-			 out.println("<a href=login.jsp>Try Again</a>");
-		 }
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3308/userdetails", "root", "password");
+			// Step 4: implement the sql query using prepared statement
+			// (https://docs.oracle.com/javase/tutorial/jdbc/basics/prepared.html)
+			PreparedStatement ps = con
+					.prepareStatement("select username from USERDETAILS where username=? and password=?");
+			// Step 5: parse in the data retrieved from the web form request into the
+			// prepared statement accordingly
+			ps.setString(1, n);
+			ps.setString(2, p);
+			// Step 6: perform the query on the database using the prepared statement
+			ResultSet rs = ps.executeQuery();
+			// Step 7: check if the query had been successfully execute, return “You are
+			// successfully registered” via the response,
+			if (rs.next()) {
+				RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+				rd.forward(request, response);
+			} else {
+				out.println("Login Failed!");
+				out.println("<a href=login.jsp>Try Again</a>");
+			}
 		}
-		//Step 8: catch and print out any exception
+		// Step 8: catch and print out any exception
 		catch (ClassNotFoundException e) {
-		 
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -1,70 +1,79 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
 <title>Login</title>
 <style>
-.container{
-    border: 1px solid #29465B;
-    border-radius: 1rem;
-    margin-left: 3rem;
-    margin-top: 1rem;
-    padding: 2rem;
-    padding-right: -200rem;
-    width: 30%;
-    margin-bottom: 2rem;
+.container {
+	border: 1px solid #29465B;
+	border-radius: 1rem;
+	margin-left: 3rem;
+	margin-top: 1rem;
+	padding: 2rem;
+	padding-right: -200rem;
+	width: 30%;
+	margin-bottom: 2rem;
 }
-.details{
-    width: 28rem;
+
+.details {
+	width: 28rem;
 }
-form{
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+
+form {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
 }
-button{
-    border-radius: 1rem;
-    border: none;
-    padding: .5rem;
-    color: white;
-    opacity: .7;
-    outline: none;
-    margin-left: 0px;
-    background-color: #29465B;
-    width: 10rem;
-    margin-bottom: 1rem;
-    
+
+button {
+	border-radius: 1rem;
+	border: none;
+	padding: .5rem;
+	color: white;
+	opacity: .7;
+	outline: none;
+	margin-left: 0px;
+	background-color: #29465B;
+	width: 10rem;
+	margin-bottom: 1rem;
 }
-.user{
-    color:#000000
+
+.user {
+	color: #000000
 }
 </style>
 </head>
 <body>
-<%@ include file = "header.jsp" %>
-<div class="container">
-    <div class="details">
-        <h2>Login</h2>
-        <form action="LoginServlet" method="post">
-        
-        <div class="input-container">
-		<label style="margin-right: 2em;">Username:</label><br><input style="width: 90%;" type="text" name="YourUsername" size="25">
-	</div>
+	<%@ include file="header.jsp"%>
+	<div class="container">
+		<div class="details">
+			<h2>Login</h2>
+			<form action="LoginServlet" method="post">
 
-            <div class="input-container">
-		<label style="margin-right: 2em;">Password:</label><br><input style="width: 90%;" type="password" name="YourPassword" size="20">
-	</div>
-            <br>
-<div class="button"></div>
-<button type="submit" value="Login!">Login</button>
-        </form>
-        <a class="user" href="register.jsp">New user? Register here</a>
-    </div>
-    <br><br><br>
+				<div class="input-container">
+					<label style="margin-right: 2em;">Username:</label><br>
+					<input style="width: 90%;" type="text" name="YourUserName"
+						size="25">
+				</div>
 
-</div>
-<%@ include file = "footer.jsp" %>
+				<div class="input-container">
+					<label style="margin-right: 2em;">Password:</label><br>
+					<input style="width: 90%;" type="password" name="YourPassword"
+						size="20">
+				</div>
+				<br>
+				<div class="button"></div>
+				<button type="submit" value="Login!">Login</button>
+			</form>
+			<a class="user" href="register.jsp">New user? Register here</a>
+		</div>
+		<br>
+		<br>
+		<br>
+
+	</div>
+	<%@ include file="footer.jsp"%>
 </body>
 </html>
