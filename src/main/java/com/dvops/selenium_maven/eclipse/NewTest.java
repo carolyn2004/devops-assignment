2f3bc3a9f1a477f52c55af61d540a2b1e7d731fd
carolyn2004 <2102332J@student.tp.edu.sg>
2022-12-09 21:53:02 +0800

    [feat](selenium): Integrated maven web project with selenium
    - Configured pom.xml file to retrieve the required Selenium dependencies
    from maven repositories.
    - Created a new TestNG Class.
    
    Signed-off-by: carolyn2004 <2102332J@student.tp.edu.sg>

---
+++ src/main/java/com/dvops/selenium_maven/eclipse/NewTest.java
@@ -0,0 +1,19 @@
+package com.dvops.selenium_maven.eclipse;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
